533e050d8211d7ceaf8535e89c54321fbc1b4f54
andreasma <maand@gmx.de>
2022-01-05 20:38:52 +0100

    add different cases

--- src/reducers/categories/categories.js
+++ src/reducers/categories/categories.js
@@ -8,6 +8,30 @@ const initialState = {
 
   
 export default function categories(state = initialState, action = {}) {
-
-    return state;
-    }
+  switch (action.type) {
+    case `${GET_CATEGORIES}_PENDING`:
+      return {
+        ...state,
+        error: null,
+        loaded: false,
+        loading: true,
+      };
+    case `${GET_CATEGORIES}_SUCCESS`:
+      return {
+        ...state,
+        ...action.result,
+        error: null,
+        loaded: true,
+        loading: false,
+      };
+    case `${GET_CATEGORIES}_FAIL`:
+      return {
+        ...state,
+        error: action.error,
+        loaded: false,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
