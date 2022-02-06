99cd9a33808ad79f5e5b1c0712fc66e06f2c5db2
andreasma <maand@gmx.de>
2022-02-06 10:11:36 +0100

    add missing imports and exports after Volto update

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,9 @@
  *   Footer,
  * };
  */
+import TlcenterView from './Views/Tlcenter';
+import TlprojectView from './Views/Tlproject';
+import Categories from './Categories/categories';
+import OwnProjects from './Ownprojects/ownprojects';
+ 
+export { TlcenterView, TlprojectView, Categories, OwnProjects };
